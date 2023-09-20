096a9991c0defbcdd1fa5705fb4fbc3d66bf6fb5
Hornyák <bence.hornyak63@gmail.com>
2023-09-20 18:36:29 +0200

    Took out some comments

--- script.js
+++ script.js
@@ -2,41 +2,45 @@ function logger(text) {
     console.log(text)
 }
 
+/* radical function */
 
 function radicalNumbers (a){
-    let radicalNumbers = Math.sqrt(a);
-    return radicalNumbers;
+    let rn = Math.sqrt(a);
+    return rn;
 }
 
 logger(radicalNumbers(4))
 
-
+/* square rooting function */
 
 function squareNumbers (b){
-    let squareNumbers = b * b;
-    return squareNumbers;
+    let sn = b * b;
+    return sn;
 }
 
 
 logger(squareNumbers(10))
 
-
+/* centimeter to inch function */
 
 function toCentimeter(inch) {
-    let toCentimeter = inch * 2.54;
-    return toCentimeter;
+    let tc = 0;
+    tc = inch * 2.54;
+    return tc;
 }
 
-logger(toCentimeter(7))
+logger(toCentimeter(5))
 
+/* able to buy alcohol function */
 
+/* just trying boolean with the use of w3 */
 
-function sellingAlcohol (a){ /* just trying boolean with the use of w3 */
+function sellingAlcohol (a){ 
     if (a >= 18) {
-        return tex = "Can buy alcohol";
+        return text = "Can buy alcohol";
     }
     else if (a < 18)
-        return text ="Too young to buy alcohol";
+        return text ="Too young to buy alcohol boohoo";
 }
 
 logger(sellingAlcohol(5))
