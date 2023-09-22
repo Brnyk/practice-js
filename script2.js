267b95dc2a9745ee94d68b20ef6b8f8c23f12e90
Hornyák <bence.hornyak63@gmail.com>
2023-09-22 17:26:08 +0200

    Added some more data in js

--- script2.js
+++ script2.js
@@ -5,8 +5,65 @@ let localBeerFactories = {
             established: 1973,
             priceAvg: 482,
             alcAvg: 4.5,
-            ownBeers:  ["Borsodi sör", "Borsodi Bivaly", "Rákóczi sör","Borsody Hoppy", "Borsodi Mester", "Borsodi IPA", "Borsodi Gyömbér Ale", "Borsodi Meggy Ale", "Borsodi Tropical Ale" ],
-            licensedBeers: ["Stella Artois", "Leffe","Beck's","Löwenbrau","Belle-Vue", "Hoegaarden", "Spaten", "Riesenbrau"]
+            ownBeers:  [
+                {
+                    beerName: "Borsodi",
+                    price: 350,
+                    abv: 4.5
+                }, 
+
+                { 
+                    beerName: "Borsodi Bivaly",
+                    price: 380,
+                    abv: 5,
+                },
+
+                { 
+                    beerName: "Rákóczi sör",
+                    price: 300,
+                    abv: 4.6,
+                },
+
+                { 
+                    beerName: "Borsody Hoppy",
+                    price: 380,
+                    abv: 4.5,
+                },
+
+                { 
+                    beerName: "Borsodi Mester",
+                    price: 400,
+                    abv: 5.5,
+                },
+
+                { 
+                    beerName: "Borsodi IPA",
+                    price: 390,
+                    abv: 4.8,
+                }
+            ],
+            licensedBeers: [
+                { 
+                    beerName: "Stella Artois",
+                    price: 365,
+                    abv: 4,
+                },
+                { 
+                    beerName: "Beck's",
+                    price: 400,
+                    abv: 4.5,
+                },
+                { 
+                    beerName: "Löwenbrau",
+                    price: 280,
+                    abv: 3.5,
+                }, 
+                { 
+                    beerName: "Hoegaarden",
+                    price: 450,
+                    abv: 4,
+                },
+            ]
         },
 
         {
@@ -14,8 +71,40 @@ let localBeerFactories = {
             established: 1854,
             priceAvg: 404,
             aclAvg: 4.7,
-            ownBeers:["Dreher Gold", "Arany Ászok","Kőbányai"],
-            licensedBeers: ["Pilsner Urquell", "Kozel", "Asahi", "Hofbräu Lager"],
+            ownBeers:[
+                { 
+                    beerName: "Dreher Gold",
+                    price: 400,
+                    abv: 4.5,
+                },    
+                {
+                    beerName: "Arany Ászok",
+                    price: 330,
+                    abv: 4.6,
+                },
+                {
+                    beerName: "Kőbányai",
+                    price: 275,
+                    abv: 4.5,
+                },
+            ],
+            licensedBeers: [
+                { 
+                    beerName: "Pilsner Urquell",
+                    price: 330,
+                    abv: 4.5,
+                }, 
+                {
+                    beerName: "Kozel",
+                    price: 380,
+                    abv: 4,
+                }, 
+                {
+                    beerName: "Asahi",
+                    price: 450,
+                    abv: 3.5,
+                },
+            ],
             
         },
         
@@ -24,13 +113,55 @@ let localBeerFactories = {
             priceAvg: 465,
             alcAvg: 5,
             established: 1895,
-            ownBeers: ["Heineken", "Soproni", "Gösser", "Arany Hordó", "Sárkány", "Arany Fácán"],
-            licensedBeers: ["Kaiser", "Edelweiss", "Steffl", "Strongbow", "Krusovice", "Adambrau", "Zlaty Bazant"]
+            ownBeers: [
+                {
+                    beerName: "Heineken",
+                    price: 400,
+                    abv: 5,
+                },
+                {
+                    beerName: "Soproni",
+                    price: 300,
+                    abv: 4.5,
+                },
+                {  
+                    beerName: "Gösser",
+                    price: 230,
+                    abv: 3.7,
+                },
+                { 
+                    beerName: "Arany Fácán",
+                    price: 250,
+                    abv: 3.5,
+                },
+            ],
+            licensedBeers: [
+                { 
+                    beerName:"Kaiser",
+                    price: 320,
+                    abv: 3,
+                },
+                {
+                    beerName: "Edelweiss",
+                    price: 600,
+                    abv: 5.5,
+                },
+                { 
+                    beerName: "Krusovice",
+                    price: 450,
+                    abv: 5,
+                },
+                {
+                    beerName: "Zlaty Bazant",
+                    price: 450,
+                    abv: 4,
+                },
+            ]
         },
     ],
 };
 
-console.log(localBeerFactories.factoryData[0].licensedBeers[3])
+console.log(localBeerFactories.factoryData[0].licensedBeers[2].beerName)
 
 /* function decideIfBeerIsGood(){
     if (priceAvg < 466 || alcAvg > 4.5){
